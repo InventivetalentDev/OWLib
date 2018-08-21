6c9034545ccd9d6e340d70f55511d6b8bd326066
Js41637 <Js41637@users.noreply.github.com>
2018-08-21 21:26:07 +1000

    [DumpVoice] add skin specific voicelines

--- DataTool/ToolLogic/Dump/DumpHeroVoice.cs
+++ DataTool/ToolLogic/Dump/DumpHeroVoice.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using DataTool.DataModels;
 using DataTool.Flag;
+using DataTool.SaveLogic.Unlock;
 using static DataTool.Program;
 using static DataTool.Helper.STUHelper;
 using Newtonsoft.Json;
@@ -12,6 +13,7 @@ using Combo = DataTool.FindLogic.Combo;
 using static DataTool.Helper.IO;
 using STUHero = TankLib.STU.Types.STUHero;
 using STUVoiceSetComponent = TankLib.STU.Types.STUVoiceSetComponent;
+using static DataTool.Helper.Logger;
 
 namespace DataTool.ToolLogic.Dump {
     [Tool("dump-hero-voice", Description = "Dumps all the strings", TrackTypes = new ushort[] { 0x75 }, CustomFlags = typeof(DumpFlags))]
@@ -24,21 +26,23 @@ namespace DataTool.ToolLogic.Dump {
             public string HeroName;
             public string SoundFile;
             public string StimulusSet;
-            public string ConversationSet;
-            public int? ConversationPos;
+            public string ConvoSet;
+            public int? ConvoPos;
             public string Subtitle;
+            public string[] Skins;
 
             [JsonIgnore]
             public ulong GUID;
 
-            public SoundInfo(string heroName, ulong guid, ulong groupGuid, string subtitle, ulong conversationGuid, int? conversationPosition) {
+            public SoundInfo(string heroName, ulong guid, ulong groupGuid, string subtitle, ulong convoGuid, int? convoPosition, string skin) {
                 GUID = guid;
                 HeroName = heroName;
                 SoundFile = $"{teResourceGUID.LongKey(guid):X12}";
                 StimulusSet = $"{teResourceGUID.LongKey(groupGuid):X12}";
-                ConversationSet = conversationGuid == 0 ? null : $"{teResourceGUID.LongKey(conversationGuid):X12}";
-                ConversationPos = conversationPosition;
+                ConvoSet = convoGuid == 0 ? null : $"{teResourceGUID.LongKey(convoGuid):X12}";
+                ConvoPos = convoPosition;
                 Subtitle = subtitle;
+                Skins = skin != null ? new []{skin} : null;
             }
         }
 
@@ -49,46 +53,102 @@ namespace DataTool.ToolLogic.Dump {
                 STUHero hero = GetInstance<STUHero>(heroGuid);
                 if (hero == null) continue;
                 
+                var progression = new ProgressionUnlocks(hero);
+                if (progression.LootBoxesUnlocks == null) continue; // no npcs thanks
+                
                 string heroNameActual = (GetString(hero.m_0EDCE350) ?? $"Unknown{teResourceGUID.Index(heroGuid)}").TrimEnd(' ');
+                STUVoiceSetComponent baseComponent = default;
+                Combo.ComboInfo baseInfo = default;
                 
-                STUVoiceSetComponent voiceSetComponent = GetInstance<STUVoiceSetComponent>(hero.m_gameplayEntity);
+                Log("\tProcessing data for {0}", heroNameActual);
 
-                if (voiceSetComponent?.m_voiceDefinition == null) continue;
-                
-                Combo.ComboInfo info = new Combo.ComboInfo();
-                Combo.Find(info, voiceSetComponent.m_voiceDefinition);
-                
-                VoiceSet voiceSet = new VoiceSet(GetInstance<STUVoiceSet>(voiceSetComponent.m_voiceDefinition));
-                Combo.VoiceSetInfo voiceSetInfo = info.VoiceSets[voiceSetComponent.m_voiceDefinition];
-              
-                foreach (var voicelineInstanceInfo in voiceSetInfo.VoiceLineInstances) {
-                    foreach (var voiceLineInstance in voicelineInstanceInfo.Value) {
-                        var subtitleInfo = GetInstance<STU_7A68A730>(voiceLineInstance.Subtitle);
-                        var subtitle = subtitleInfo?.m_798027DE.m_text;
-                        ulong conversationGuid = 0;
-                        int? conversationPosition = null;
+                if (ProcessSounds(heroNameActual, hero.m_gameplayEntity, null, ref baseComponent, ref baseInfo, ref soundList)) {
+                    if (hero.m_heroProgression == 0) continue;
+                    
+                    foreach (Unlock itemInfo in progression.OtherUnlocks)
+                        ProcessUnlock(heroNameActual, itemInfo, hero, baseComponent, baseInfo, ref soundList);
 
-                        if (voiceSet.VoiceLines.ContainsKey(voiceLineInstance.GUIDx09B)) {
-                           var vl = voiceSet.VoiceLines[voiceLineInstance.GUIDx09B];
+                    foreach (var defaultUnlocks in progression.LevelUnlocks)
+                        foreach (Unlock unlock in defaultUnlocks.Unlocks)
+                            ProcessUnlock(heroNameActual, unlock, hero, baseComponent, baseInfo, ref soundList);
 
-                            if (vl.VoiceConversation != 0) {
-                                var convo = GetInstance<STUVoiceConversation>(vl.VoiceConversation);
-                                conversationPosition = convo.m_voiceConversationLine.ToList().FindIndex(c => c.m_lineGUID == voiceLineInstance.GUIDx09B);
-                                conversationGuid = vl.VoiceConversation;
-                            }
-                        }
+                    foreach (var eventUnlocks in progression.LootBoxesUnlocks) 
+                    {
+                        if (eventUnlocks?.Unlocks == null) continue;
 
-                        foreach (var sound in voiceLineInstance.SoundFiles) {
-                            soundList.Add(new SoundInfo(heroNameActual, sound, voiceLineInstance.VoiceStimulus, subtitle, conversationGuid, conversationPosition));
-                        }
+                        foreach (Unlock unlock in eventUnlocks.Unlocks)
+                            ProcessUnlock(heroNameActual, unlock, hero, baseComponent, baseInfo, ref soundList);
                     }
                 }
             }
-            
+
             ParseJSON(
                 soundList.OrderBy(s => s.GUID).ToList(),
                 toolFlags as DumpFlags
             );
+        }
+        
+        public static void ProcessUnlock(string heroNameActual, Unlock unlock, STUHero hero, STUVoiceSetComponent baseComponent, Combo.ComboInfo baseInfo, ref List<SoundInfo> soundList) {
+            if (!(unlock.STU is STUUnlock_SkinTheme unlockSkinTheme)) return;
+            if (unlockSkinTheme.m_0B1BA7C1 != 0)
+                return;
+
+            SaveSkin(heroNameActual, unlockSkinTheme.m_skinTheme, hero, unlock.Name, baseComponent, baseInfo, ref soundList);
+        }
+        
+        public static void SaveSkin(string heroNameActual, ulong skinResource, STUHero hero, string name, STUVoiceSetComponent baseComponent, Combo.ComboInfo baseInfo, ref List<SoundInfo> soundList)
+        {
+            STUSkinTheme skin = GetInstance<STUSkinTheme>(skinResource);
+            if (skin == null)
+                return;
+
+            STUVoiceSetComponent component = default;
+            Combo.ComboInfo info = default;
+
+            ProcessSounds(heroNameActual, hero.m_gameplayEntity, name, ref component, ref info, ref soundList, baseComponent, baseInfo, SkinTheme.GetReplacements(skin));
+        }        
+
+        public static bool ProcessSounds(string heroNameActual, ulong entityMain, string skin, ref STUVoiceSetComponent voiceSetComponent,
+            ref Combo.ComboInfo info, ref List<SoundInfo> soundList, STUVoiceSetComponent baseComponent = null, Combo.ComboInfo baseCombo = null,
+            Dictionary<ulong, ulong> replacements = null) {
+            voiceSetComponent = GetInstance<STUVoiceSetComponent>(Combo.GetReplacement(entityMain, replacements));
+
+            if (voiceSetComponent?.m_voiceDefinition == null)
+                return false;
+
+            info = new Combo.ComboInfo();
+            Combo.Find(info, voiceSetComponent.m_voiceDefinition, replacements);
+
+            if (baseComponent != null && baseCombo != null)
+                if (!Combo.RemoveDuplicateVoiceSetEntries(baseCombo, ref info, baseComponent.m_voiceDefinition, Combo.GetReplacement(voiceSetComponent.m_voiceDefinition, replacements)))
+                    return false;
+            
+            VoiceSet voiceSet = new VoiceSet(GetInstance<STUVoiceSet>(Combo.GetReplacement(voiceSetComponent.m_voiceDefinition, replacements)));
+            Combo.VoiceSetInfo voiceSetInfo = info.VoiceSets[Combo.GetReplacement(voiceSetComponent.m_voiceDefinition, replacements)];
+            
+            foreach (var voicelineInstanceInfo in voiceSetInfo.VoiceLineInstances) {
+                foreach (var voiceLineInstance in voicelineInstanceInfo.Value) {
+                    var subtitleInfo = GetInstance<STU_7A68A730>(voiceLineInstance.Subtitle);
+                    var subtitle = subtitleInfo?.m_798027DE.m_text;
+                    ulong conversationGuid = 0;
+                    int? conversationPosition = null;
+
+                    if (voiceSet.VoiceLines.ContainsKey(voiceLineInstance.GUIDx09B)) {
+                        var vl = voiceSet.VoiceLines[voiceLineInstance.GUIDx09B];
+
+                        if (vl.VoiceConversation != 0) {
+                            var convo = GetInstance<STUVoiceConversation>(vl.VoiceConversation);
+                            conversationPosition = convo.m_voiceConversationLine.ToList().FindIndex(c => c.m_lineGUID == voiceLineInstance.GUIDx09B);
+                            conversationGuid = vl.VoiceConversation;
+                        }
+                    }
+
+                    foreach (var sound in voiceLineInstance.SoundFiles) {
+                        soundList.Add(new SoundInfo(heroNameActual, sound, voiceLineInstance.VoiceStimulus, subtitle, conversationGuid, conversationPosition, skin));
+                    }
+                }
+            }
+            return true;
         }
     }
 }
